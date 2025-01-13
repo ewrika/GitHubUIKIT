8d5cf56a9e208c4417299cbea2523ec780e9642e
ewrika <egorkaomsk_2003@mail.ru>
2025-01-14 00:14:29 +0600

    Model+Fix

---
+++ GitHubUIKIT/Model/Follower.swift
@@ -0,0 +1,12 @@
+//
+//  Follower.swift
+//  GitHubUIKIT
+//
+//  Created by Георгий Борисов on 13.01.2025.
+//
+
+import Foundation
+struct Follower:Codable {
+    var login:String
+    var avatarUrl:String
+}
---
+++ GitHubUIKIT/Model/User.swift
@@ -0,0 +1,23 @@
+//
+//  User.swift
+//  GitHubUIKIT
+//
+//  Created by Георгий Борисов on 13.01.2025.
+//
+
+import Foundation
+
+struct User:Codable{
+    var login:String
+    var avatarUrl:String
+    var name:String?
+    var location:String?
+    var bio:String?
+    var publicRepos:Int
+    var publicGists:Int
+    var htmlUrl:String
+    var following:Int
+    var followers:Int
+    var createAt: String
+    
+}
