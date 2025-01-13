58cf237934a351ce1e9c85aef801a05b8b19b4df
ewrika <egorkaomsk_2003@mail.ru>
2025-01-14 01:40:40 +0600

    NetworkCall-Get Followers

--- GitHubUIKIT/Screens/FollowerListVC.swift
+++ GitHubUIKIT/Screens/FollowerListVC.swift
@@ -16,9 +16,17 @@ class FollowerListVC: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
 
+        NetworkManager.shared.getFollowers(for: username, page: 1) { (followers , errorMessage) in
+            guard let followers = followers else {
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: errorMessage!, buttonTitle: "Ok")
+                return
+            }
+            print("Followers.cont = \(followers.count)")
+            print(followers)
+        }
     }
-    
-    
+
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
