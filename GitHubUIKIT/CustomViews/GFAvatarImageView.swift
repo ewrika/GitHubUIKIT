159d8f4ca0986ecb6d46f7f759bc10e86f70cf1e
ewrika <egorkaomsk_2003@mail.ru>
2025-01-14 15:28:47 +0600

    CustomFollowerCell

---
+++ GitHubUIKIT/CustomViews/GFAvatarImageView.swift
@@ -0,0 +1,30 @@
+//
+//  GFAvatarImageView.swift
+//  GitHubUIKIT
+//
+//  Created by Георгий Борисов on 14.01.2025.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+
+    let placeholderImage = UIImage(named:"avatar-placeholder")!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
