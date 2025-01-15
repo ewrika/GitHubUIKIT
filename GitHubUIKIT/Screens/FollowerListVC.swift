84bc5c03d1209b74dc3f165721b0457f91683d30
ewrika <egorkaomsk_2003@mail.ru>
2025-01-16 01:27:51 +0600

    Sear Functionality

--- GitHubUIKIT/Screens/FollowerListVC.swift
+++ GitHubUIKIT/Screens/FollowerListVC.swift
@@ -14,6 +14,7 @@ class FollowerListVC: UIViewController {
 
     var username: String!
     var followers: [Follower] = []
+    var filteredFollowers: [Follower] = []
     var page = 1
     var hasMoreFollowers = true
 
@@ -24,6 +25,7 @@ class FollowerListVC: UIViewController {
         super.viewDidLoad()
         configureViewController()
         configureCollectionView()
+        configureSearchController()
         getFollower(username: username, page: page)
         configureDataSource()
     }
@@ -44,6 +46,16 @@ class FollowerListVC: UIViewController {
         collectionView.delegate = self
         collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    func configureSearchController() {
+        let searchController = UISearchController()
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate self
+        searchController.searchBar.placeholder = "Search for a username"
+        searchController.obscuresBackgroundDuringPresentation = false
+        navigationItem.searchController = searchController
+        
     }
 
     func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
@@ -76,7 +88,7 @@ class FollowerListVC: UIViewController {
                     }
                     return
                 }
-                self.updateData()
+                self.updateData(on:self.followers)
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "Ok")
             }
@@ -95,7 +107,7 @@ class FollowerListVC: UIViewController {
         })
     }
 
-    func updateData() {
+    func updateData(on followers: [Follower]) {
         var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
         snapshot.appendSections([.main])
         snapshot.appendItems(followers)
@@ -121,5 +133,17 @@ extension FollowerListVC: UICollectionViewDelegate {
             page += 1
             getFollower(username: username, page: page)
         }
+    }
+}
+
+extension FollowerListVC:UISearchResultsUpdating,UISearchBarDelegate {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let filter = searchController.searchBar.text , !filter.isEmpty else { return }
+        
+        filteredFollowers = followers.filter { $0.login.lowercased().contains(filter.lowercased()) }
+        updateData(on: filteredFollowers)
+    }
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        updateData(on: followers)
     }
 }
